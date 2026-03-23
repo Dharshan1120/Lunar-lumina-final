5261c9fae7ccc41f9aecfe2edcccb02ae9d94bb2
Dharshan1120 <dharshansmd@gmail.com>
2026-03-23 21:45:18 +0530

    firebase

--- upflux/src/services/firebase.js
+++ upflux/src/services/firebase.js
@@ -6,9 +6,9 @@ import { connectFirestoreEmulator, initializeFirestore } from "firebase/firestor
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCK93RXiaaynzRrQA1Hu9tzkBttgb1N2J8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "learnova-ai.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "learnova-ai",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "learnova-ai.firebasestorage.app",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "upflux-ai.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "upflux-ai",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "upflux-ai.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "991438837299",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:991438837299:web:ed000930fcc7cf1a28f546"
 };
